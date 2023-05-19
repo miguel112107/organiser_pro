4da8697f5f488152608ed1c61c2bfa4a0e67d3cc
miguel112107 <maflores1108@gmail.com>
2023-05-18 18:13:40 -0600

    seeded script for all seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,10 +1,9 @@
 <?php
-
+  
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+  
 use Illuminate\Database\Seeder;
-
+  
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSseeder::class,
+            BarPlansSeeder::class,
+            DinnerStylesSeeder::class,
+            LinenChoicesSeeder::class,
+            LocationsSeeder::class,
+            PackagesSeeder::class,
+            TableLayoutsSeeder::class
+        ]);
     }
 }
